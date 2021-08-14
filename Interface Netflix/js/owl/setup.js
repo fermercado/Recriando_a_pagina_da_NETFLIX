eaf137e35a4a6646bdb5ef2d7135528369487cf8
Fernando Mercado <88064355+fermercado@users.noreply.github.com>
2021-08-14 17:00:08 -0300

    Add files via upload

---
+++ Interface Netflix/js/owl/setup.js
@@ -0,0 +1,79 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+            
+        }
+    }
+})
+
+function clicou(){
+    window.open("https://www.themoviedb.org/tv/103768-sweet-tooth?language=pt-BR")
+    //window.location.href = ("https://www.themoviedb.org/tv/103768-sweet-tooth?language=pt-BR")
+}
+
+function clicouTrailer(){
+    window.open("https://www.youtube.com/watch?v=0mz_ImwSOpM");
+ 
+
+}
+function filme1(){
+    window.open("https://www.youtube.com/watch?v=Yj0l7iGKh8g");
+ 
+
+}
+function filme2(){
+    window.open("https://www.youtube.com/watch?v=Uv8bfaxl9j4");
+ 
+
+}
+function filme3(){
+    window.open("https://www.youtube.com/watch?v=ZHaokMTWgdY");
+ 
+
+}
+function filme4(){
+    window.open("https://www.youtube.com/watch?v=sw5zp8817i4");
+ 
+
+}
+function filme5(){
+    window.open("https://www.youtube.com/watch?v=qwOWJ8pPgk8");
+ 
+
+}
+function filme6(){
+    window.open("https://www.youtube.com/watch?v=Nu5z3AT2jv8");
+ 
+
+}
+function filme7(){
+    window.open("https://www.youtube.com/watch?v=yTDDG1_1h1Y");
+ 
+
+}
+function filme8(){
+    window.open("https://www.youtube.com/watch?v=Fd09zvjdktE");
+ 
+
+}
+function filme9(){
+    window.open("https://www.youtube.com/watch?v=VwOPA2upeCA");
+ 
+
+}
+function filme10(){
+    window.open("https://www.youtube.com/watch?v=xdBWrkcmMwU");
+ 
+
+}
+
